6f496c1b41935aaba146c7f8e977f6ca907377c8
BB Predator Laptop <ya.ea.fb@gmail.com>
2022-10-24 16:38:28 -0600

    daily practice

--- ex_07_1/openFileTest.py
+++ ex_07_1/openFileTest.py
@@ -1,8 +1,11 @@
 fhand = open('ex_07_1\L00099100-3.NCF')
 count = 0
+findit = input("Ask and you shall receive! What shall I find? ")
 for cheese in fhand:
-    #print(cheese)
-    count = count+1
-    #replaceorfind = cheese.find(input('what would you like to find? ')) # not working yet
-    #print(replaceorfind) # not working yet
-print('Line count', count)
+    #count = count+1 ## used if you want to count the number of lines
+    if cheese.startswith(findit):
+                print(cheese)
+#print('Line count', count) ## print the number of lines
+#wholefile = fhand.read() # .read comand reads the whole file
+#print(wholefile)
+#print(len(wholefile))
