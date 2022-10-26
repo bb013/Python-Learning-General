e70b40db0b70e4c13447613e64344682c0ec2401
brettb013 <ya.ea.fb@gmail.com>
2022-10-26 16:40:43 -0600

    sick day play

---
+++ ex_08_lists/08-lists-1.py
@@ -0,0 +1,9 @@
+friendslist = ['Joe', 'Bob', 'Billy', 'Jean']
+for i in range(len(friendslist)):
+    friendname = friendslist[i]
+    print('Hello',friendname,'my friend!')
+#for x in friendslist: # does the same thing as previous for loop, but simpler. There are cases for each
+#    print('Hello',x,'my friend!')
+#print(range(4))
+# print(len(friendslist))
+#print(range(len(friendslist)))
