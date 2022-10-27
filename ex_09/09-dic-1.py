058c9b9676eaf9c63e7ae8926f77efb6b6a53898
BB Predator Laptop <ya.ea.fb@gmail.com>
2022-10-27 15:41:56 -0600

    some dictionary stuff - needs review

--- ex_09/09-dic-1.py
+++ ex_09/09-dic-1.py
@@ -2,11 +2,25 @@
 #counts = { 'quincy' : 1 , 'mrugesh' : 42, 'beau': 100, '0': 10}
 #print(counts.get('kris', 0))
 #### End video question code
-dicname = dict()
+
+### first example tried in video
+#dictinfo = dict()
+#names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
+#for data in names:
+#    if data not in dictinfo:
+#        dictinfo[data] = 1
+#    else:
+#        dictinfo[data]=dictinfo[data]+1
+#print(dictinfo)
+### end first example
+
+counts = dict()
 names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
-for datainput in names:
-    if datainput not in dicname:
-        dicname[datainput] = 1
-    else:
-        dicname[datainput]=dicname[datainput]+1
-print(dicname)
+for name in names:
+    counts[name] = counts.get(name, 0) + 1
+#    if name in counts:
+#        x = counts[name]
+#    else:
+#        x = 0
+#x = counts.get(name, 0)
+print(counts)
