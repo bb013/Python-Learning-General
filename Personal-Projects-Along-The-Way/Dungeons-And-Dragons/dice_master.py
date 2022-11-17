eacdf4e7479cae3d25ca3e46d79b215391db81b7
BB Predator Laptop <ya.ea.fb@gmail.com>
2022-11-17 16:36:38 -0700

    Work related to a dice engine

---
+++ Personal-Projects-Along-The-Way/Dungeons-And-Dragons/dice_master.py
@@ -0,0 +1,31 @@
+import random
+
+# generates random number to simulate different dice rolls
+def die_d4():
+    #returns a random number in stated range
+    d4 = random.randint(1,4)
+    return d4
+def die_d6():
+    #returns a random number in stated range
+    d6 = random.randint(1,6)
+    return d6
+def die_d8():
+    #returns a random number in stated range
+    d8 = random.randint(1,8)
+    return d8
+def die_d10():
+    #returns a random number in stated range
+    d10 = random.randint(1,10)
+    return d10
+def die_d12():
+    #returns a random number in stated range
+    d12 = random.randint(1,12)
+    return d12
+def die_d20():
+    #returns a random number in stated range
+    d20 = random.randint(1,20)
+    return d20
+def die_d100():
+    #returns a random number in stated range
+    d100 = random.randint(1,100)
+    return d100
