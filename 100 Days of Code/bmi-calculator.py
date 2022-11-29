dc3195aa2f9efce8257c921cfd40504f40e8f06a
brettb013 <ya.ea.fb@gmail.com>
2022-11-28 20:55:26 -0700

    Day 3 exercises

--- 100 Days of Code/bmi-calculator.py
+++ 100 Days of Code/bmi-calculator.py
@@ -4,5 +4,31 @@ weight = input("enter your weight in kg: ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-bmi = int(float(weight) / float(height) **2)
-print(bmi)
+bmi = round(float(weight) / float(height) **2)
+if bmi <= 18.5:
+    statement = "you are underweight"
+elif bmi <= 25:
+    statement = "you have a normal weight"
+elif bmi <= 30:
+    statement = "you are slightly overweight"
+elif bmi <= 35:
+    statement = "you are obese"
+elif bmi > 35:
+    statement = "you are clinically obese"
+print(f"Your BMI is {bmi}, {statement}.")
+
+
+# "Your BMI is 18, you are underweight."
+# "Your BMI is 22, you have a normal weight."
+# "Your BMI is 28, you are slightly overweight."
+# "Your BMI is 33, you are obese."
+# "Your BMI is 40, you are clinically obese."
+
+# print(bmi)
+# It should tell them the interpretation of their BMI based on the BMI value.
+
+#     Under 18.5 they are underweight
+#     Over 18.5 but below 25 they have a normal weight
+#     Over 25 but below 30 they are slightly overweight
+#     Over 30 but below 35 they are obese
+#     Above 35 they are clinically obese.
