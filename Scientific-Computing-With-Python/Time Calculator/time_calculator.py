37df881f62bf8e28fa61f5b4e0a430540a429b7c
BB Predator Laptop <ya.ea.fb@gmail.com>
2023-01-12 15:41:14 -0700

    finished time_calculator.py
    passes all tests

--- Scientific-Computing-With-Python/Time Calculator/time_calculator.py
+++ Scientific-Computing-With-Python/Time Calculator/time_calculator.py
@@ -1,5 +1,3 @@
-# Status = probably 80% complete with two TODO's left
-
 def add_time(start, duration, weekday = None):
     # rename variables for clarity
     original_time = start
@@ -14,61 +12,80 @@ def add_time(start, duration, weekday = None):
     # find hours, minutes, and am or pm of starting time
     original_time_split = original_time.split()
     am_pm_var = original_time_split[-1].upper()
+    if am_pm_var == "AM":
+        am = True
+    else: am = False
 
+    # split things up and name
     original_time_hours_minutes = original_time_split[0].split(":")
     original_hours = int(original_time_hours_minutes[0])
     original_minutes = int(original_time_hours_minutes[1])
 
-    
     # find hours, minutes of duration time
     time_to_add_hours_minutes = time_to_add.split(":")
     time_to_add_hours = int(time_to_add_hours_minutes[0])
     time_to_add_minutes = int(time_to_add_hours_minutes[1])
 
+    # 24 hour formatting
+    if am == False:
+        original_hours += 12
+    if (am == True) and (original_hours == 12):
+        original_hours -= 12
+
+    # adds hours together
+    totaled_hours = original_hours + time_to_add_hours
+
     # add minutes and if needed add to hours
     totaled_minutes = original_minutes + time_to_add_minutes
     while totaled_minutes > 59:
         totaled_minutes -= 60
-        time_to_add_hours += 1
+        totaled_hours += 1
 
-    # Covert to 24 hour format
-    if am_pm_var == 'PM':
-        original_hours += 12
-    
-    # adds hours together
-    totaled_hours = original_hours + time_to_add_hours
-    
     # how many days of time added
     days_added = 0
-    while totaled_hours > 23:
+    while totaled_hours >= 24:
         totaled_hours -= 24
         days_added += 1
 
-    # what day of week it now is
-    if weekday != None:
-        weekday_index_before_cleanup = original_weekday_indexed + days_added
-        while weekday_index_before_cleanup > 6:
-            weekday_index_before_cleanup -= 7
-        final_weekday = weekday_index_before_cleanup
+    # final am pm state
+    if totaled_hours >= 12:
+        final_am_pm_state = "PM"
+    else: final_am_pm_state = "AM"
 
-    # converts to 12 hour format
+    # tweak the final displayed hours
     if totaled_hours > 12:
         totaled_hours -= 12
-        am_pm_var = 'PM'
-    else:
-        am_pm_var = 'AM'
-    
-    # TODO: verify that these outputs match what is needed
+    if totaled_hours == 0:
+        totaled_hours = 12
+
+    # what day of week it now is
+    if weekday != None:
+        weekday_index = original_weekday_indexed + days_added
+        while weekday_index > 6:
+            weekday_index -= 7
+        final_weekday = weekday_list[weekday_index]
+
     # next day or not
     if days_added > 0:
         if days_added == 1:
-            days_later_var = "next day"
+            days_later_var = "(next day)"
         else:
-            days_later_var = str(days_added)+" days later"
-    else: days_later_var = "same day"
+            days_later_var = "("+str(days_added)+" days later)"
 
-    # TODO: create variable "new_time" with everything in it. Take into account if there is or is not a weekday input
+    # usable number formatting for minutes
+    formatted_minutes = "{0:0>2}".format(totaled_minutes)
+    
+    if (days_added == 0):
+        if weekday != None:
+            new_time = f"{totaled_hours}:{formatted_minutes} {final_am_pm_state}, {final_weekday}"
+        else:
+            new_time = f"{totaled_hours}:{formatted_minutes} {final_am_pm_state}"
+    else:
+        if weekday != None:
+            new_time = f"{totaled_hours}:{formatted_minutes} {final_am_pm_state}, {final_weekday} {days_later_var}"
+        else:
+            new_time = f"{totaled_hours}:{formatted_minutes} {final_am_pm_state} {days_later_var}"
 
     return new_time
 
-print(add_time("11:06 PM", "2:02", "Monday"))
+print(add_time("8:16 PM", "466:02", "tuesday"))
